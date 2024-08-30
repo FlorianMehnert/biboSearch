c969668997b70a32fecca49c31546f6406d4bffe
florian <florian.mehnert@gmail.com>
2024-08-30 17:57:38 +0200

    add icons and due date

--- streamlit/st_beebo_search.py
+++ streamlit/st_beebo_search.py
@@ -1,8 +1,49 @@
+import base64
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+from pygments.lexer import default
 
 BASE_URL = "https://katalog.bibo-dresden.de/webOPACClient/start.do?Login=webopac&BaseURL=this"
+BASE_LOGGED_IN_URL = "https://katalog.bibo-dresden.de"
+
+from bs4 import BeautifulSoup
+import re
+
+
+def find_due_dates(html_content):
+    """
+    return the due date of the media if it is not available
+    :param html_content:
+    :return:
+    """
+    soup = BeautifulSoup(html_content, 'html.parser')
+    results = []
+
+    # Search within each hit entry
+    for row in soup.find_all('tr'):
+        text_content = row.get_text(separator=' ', strip=True)
+        # Check if "entliehen bis" is in the row
+        if "entliehen" in text_content:
+            # Extract the due date using regex (assumes the date follows "entliehen bis")
+            due_date_match = re.search(r'entliehen.*?(\d{2}\.\d{2}\.\d{4})', text_content)
+            if due_date_match:
+                due_date = due_date_match.group(1)
+                results.append((due_date))
+    return results
+
+
+def get_medium(kind_of):
+    match kind_of:
+        case "DVD":
+            return "📀"
+        case "Blu-ray Disc":
+            return "🔵"
+        case _:
+            return kind_of
+
 
 @st.cache_resource
 def search(search_term):
@@ -66,9 +107,10 @@ def search(search_term):
         # Extract the link to the DVD
         dvd_link = title_tag['href'] if title_tag else None
         kind_of_medium = a.find("img")
-        kind_of_medium = kind_of_medium.get("title") if kind_of_medium else None
-        titles.append(year + f" {kind_of_medium} " + title + f"{" ausleihbar" if ausleihbar else ""}")
 
+        kind_of_medium = kind_of_medium.get("title") if kind_of_medium else None
+        response = session.get(BASE_LOGGED_IN_URL + dvd_link)
+        titles.append(("✅ " if ausleihbar else "❌ ") + year + f" {get_medium(kind_of_medium)} " + title + f"{" ausleihbar" if ausleihbar else " " + find_due_dates(response.content)[0]}")
     return titles
 
 
